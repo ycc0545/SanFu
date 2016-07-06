44100122fbb0e3a4b6d8b971fcb17423bcd7286e
Alpha110710 <1262632118@qq.com>
2016-07-06 11:59:31 +0800

    图标

--- SanFu/app/src/main/java/com/example/dllo/sanfu/main/MainActivity.java
+++ SanFu/app/src/main/java/com/example/dllo/sanfu/main/MainActivity.java
@@ -1,9 +1,11 @@
-package com.example.dllo.sanfu;
+package com.example.dllo.sanfu.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import com.example.dllo.sanfu.R;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
