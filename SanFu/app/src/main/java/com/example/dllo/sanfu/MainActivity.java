b42e48451eadd5df22467f1f8fcceaba5a2b40cf
Alpha110710 <1262632118@qq.com>
2016-07-06 11:33:27 +0800

    aaa

--- SanFu/app/src/main/java/com/example/dllo/sanfu/MainActivity.java
+++ SanFu/app/src/main/java/com/example/dllo/sanfu/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.dllo.sanfu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView viewById;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewById = (TextView) findViewById(R.id.tv1);
     }
 }
